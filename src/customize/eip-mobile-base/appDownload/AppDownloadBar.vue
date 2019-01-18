2123ba0bc358547cbed7b08be17d77d39eb02712
huanqing.shao <shaohq@foxmail.com>
2019-01-18 08:42:16 +0800

    v0.2.20

---
+++ src/customize/eip-mobile-base/appDownload/AppDownloadBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="height: 40px;">klsdfs</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$emit('height', 40)
+  }
+}
+</script>
+
+<style>
+
+</style>
